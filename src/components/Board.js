d0a9c89b520f8265dd4f980fa034f0791232f0aa
cuong <it.thieutrancuong@gmail.com>
2023-03-18 08:48:27 +0700

    hightlight

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,13 +7,17 @@ const Board = ({ boards }) => {
   const [nextPlayer, setNextPlayer] = useState("X");
   const [location, setLocation] = useState([]);
   const [currentStep, setCurrentStep] = useState(0);
+  const [winningSquares, setWinningSquares] = useState([]);
   const [gameResult, setGameResult] = useState(null);
+
+  console.log(winningSquares);
 
   const resetGame = useCallback(() => {
     setBoard(boards);
     setNextPlayer("X");
     setLocation([]);
     setCurrentStep(0);
+    setWinningSquares([]);
     setGameResult(null);
   }, [boards]);
 
@@ -22,6 +26,7 @@ const Board = ({ boards }) => {
       // Kiểm tra hàng
       for (let i = 0; i < board.length; i++) {
         if (board[i].every((square) => square === player)) {
+          setWinningSquares(board[i].map((square, index) => [i, index]));
           return true;
         }
       }
@@ -29,6 +34,7 @@ const Board = ({ boards }) => {
       // Kiểm tra cột
       for (let i = 0; i < board.length; i++) {
         if (board.every((row) => row[i] === player)) {
+          setWinningSquares(board.map((row, index) => [index, i]));
           return true;
         }
       }
@@ -39,6 +45,7 @@ const Board = ({ boards }) => {
         leftToRight.push(board[i][i]);
       }
       if (leftToRight.every((square) => square === player)) {
+        setWinningSquares(leftToRight.map((square, index) => [index, index]));
         return true;
       }
 
@@ -48,6 +55,9 @@ const Board = ({ boards }) => {
         rightToLeft.push(board[i][board.length - i - 1]);
       }
       if (rightToLeft.every((square) => square === player)) {
+        setWinningSquares(
+          rightToLeft.map((square, index) => [index, board.length - index - 1])
+        );
         return true;
       }
 
@@ -103,14 +113,21 @@ const Board = ({ boards }) => {
       <div>
         {board.map((row, rowIndex) => (
           <div key={rowIndex} className="flex">
-            {row.map((square, colIndex) => (
-              <Square
-                key={colIndex}
-                value={square}
-                onClick={() => handleClick(rowIndex, colIndex)}
-                resetGame={resetGame}
-              />
-            ))}
+            {row.map((square, colIndex) => {
+              const isWinningSquare = winningSquares.some(
+                ([winningRow, winningCol]) =>
+                  winningRow === rowIndex && winningCol === colIndex
+              );
+              return (
+                <Square
+                  key={colIndex}
+                  value={square}
+                  onClick={() => handleClick(rowIndex, colIndex)}
+                  resetGame={resetGame}
+                  isWinningSquare={isWinningSquare}
+                />
+              );
+            })}
           </div>
         ))}
         {gameResult ? (
@@ -146,3 +163,165 @@ const Board = ({ boards }) => {
 };
 
 export default Board;
+
+// import React, { useState, useMemo, useCallback } from "react";
+// import Scrollbars from "react-custom-scrollbars-2";
+// import Square from "./Square";
+
+// const Board = ({ boards }) => {
+//   const [board, setBoard] = useState(boards);
+//   const [nextPlayer, setNextPlayer] = useState("X");
+//   const [location, setLocation] = useState([]);
+//   const [currentStep, setCurrentStep] = useState(0);
+//   const [winningSquares, setWinningSquares] = useState([]);
+//   const [gameResult, setGameResult] = useState(null);
+
+//   const checkWin = useCallback((board, player) => {
+//     // Kiểm tra hàng
+//     for (let i = 0; i < board.length; i++) {
+//       if (board[i].every((square) => square === player)) {
+//         setWinningSquares(board[i].map((square, index) => [i, index]));
+//         return true;
+//       }
+//     }
+
+//     // Kiểm tra cột
+//     for (let i = 0; i < board.length; i++) {
+//       if (board.every((row) => row[i] === player)) {
+//         setWinningSquares(board.map((row, index) => [index, i]));
+//         return true;
+//       }
+//     }
+
+//     // Kiểm tra đường chéo chính
+//     const leftToRight = [];
+//     for (let i = 0; i < board.length; i++) {
+//       leftToRight.push(board[i][i]);
+//     }
+//     if (leftToRight.every((square) => square === player)) {
+//       setWinningSquares(leftToRight.map((square, index) => [index, index]));
+//       return true;
+//     }
+
+//     // Kiểm tra đường chéo phụ
+//     const rightToLeft = [];
+//     for (let i = 0; i < board.length; i++) {
+//       rightToLeft.push(board[i][board.length - i - 1]);
+//     }
+//     if (rightToLeft.every((square) => square === player)) {
+//       setWinningSquares(
+//         rightToLeft.map((square, index) => [index, board.length - index - 1])
+//       );
+//       return true;
+//     }
+
+//     return false;
+//   }, []);
+
+//   const checkTie = useCallback((board) => {
+//     return board.every((row) => row.every((square) => square !== null));
+//   }, []);
+
+//   const resetGame = useCallback(() => {
+//     setBoard(boards);
+//     setNextPlayer("X");
+//     setLocation([]);
+//     setCurrentStep(0);
+//     setWinningSquares([]);
+//     setGameResult(null);
+//   }, [boards]);
+
+//   const handleClick = (row, col) => {
+//     if (gameResult) return;
+
+//     const newBoard = board.map((boardRow, rowIndex) => {
+//       if (rowIndex === row) {
+//         return boardRow.map((value, colIndex) => {
+//           if (colIndex === col && value === null) {
+//             return nextPlayer;
+//           }
+//           return value;
+//         });
+//       }
+//       return boardRow;
+//     });
+
+//     const newLocation = [...location, [row, col]];
+//     setBoard(newBoard);
+//     setLocation(newLocation);
+//     setCurrentStep(newLocation.length);
+//     setNextPlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+
+//     if (checkWin(newBoard, nextPlayer)) {
+//       setGameResult(`${nextPlayer} wins!`);
+//     } else if (checkTie(newBoard)) {
+//       setGameResult("Tie!");
+//     }
+//   };
+
+//   const jumpTo = useCallback(
+//     (step) => {
+//       setCurrentStep(step);
+//       setBoard(boards.slice(0, step + 1));
+//       setLocation(location.slice(0, step));
+//       setNextPlayer(step % 2 === 0 ? "X" : "O");
+//       setWinningSquares([]);
+//       setGameResult(null);
+//     },
+//     [boards, location]
+//   );
+
+//   const renderSquare = (row, col) => {
+//     const value = board[row][col];
+//     const isWinningSquare = winningSquares.some(
+//       (square) => square[0] === row && square[1] === col
+//     );
+//     return (
+//       <Square
+//         key={`${row}-${col}`}
+//         value={value}
+//         isWinningSquare={isWinningSquare}
+//         onClick={() => handleClick(row, col)}
+//       />
+//     );
+//   };
+
+//   const renderHistory = useMemo(() => {
+//     return location.map((step, move) => {
+//       const desc = move
+//         ? `Go to move #${move} (${step[0]}, ${step[1]}) `
+//         : "Go to game start";
+
+//       return (
+//         <li key={move}>
+//           <button className="history" onClick={() => jumpTo(move)}>
+//             {desc}
+//           </button>
+//         </li>
+//       );
+//     });
+//   }, [location, jumpTo]);
+
+//   return (
+//     <div className="game-board">
+//       <div className="status">
+//         {gameResult ? gameResult : `Next player: ${nextPlayer}`}
+//       </div>
+//       <Scrollbars autoHide>
+//         <div className="board-row">
+//           {board.map((row, index) => (
+//             <div key={index} className="board-row">
+//               {row.map((col, index2) => renderSquare(index, index2))}
+//             </div>
+//           ))}
+//         </div>
+//       </Scrollbars>
+//       <button className="reset" onClick={() => resetGame()}>
+//         Reset
+//       </button>
+//       <ul className="history-list">{renderHistory}</ul>
+//     </div>
+//   );
+// };
+
+// export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 
-const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick, resetGame, isWinningSquare }) => {
+  console.log(isWinningSquare);
   const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
-    onClick();
-    setClicked(true);
+    if (value === null) {
+      onClick();
+      setClicked(true);
+    }
   };
 
+  const buttonClasses = `w-[50px] h-[50px] border border-[#2596be] text-black font-bold text-xl ${
+    isWinningSquare ? "bg-yellow-200" : ""
+  }`;
+
   return (
-    <button
-      onClick={handleClick}
-      className={`w-[50px] h-[50px] border border-[#2596be] ${
-        clicked ? " text-black font-bold text-xl" : ""
-      }`}
-    >
+    <button onClick={handleClick} className={buttonClasses}>
       {value}
     </button>
   );
